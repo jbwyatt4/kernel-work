83deb967077419ea8ead2819f9fdd8581d87821e
John B. Wyatt IV <jbwyatt4@gmail.com>
2021-05-25 21:10:29 -0700

    successful stress-ng process fork

--- coresched_prctl_wrapper/main.c
+++ coresched_prctl_wrapper/main.c
@@ -1,5 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h> /* for fork */
+#include <sys/types.h> /* for pid_t */
+#include <sys/wait.h> /* for wait */
+
 #include <sys/prctl.h>
 
 int main(int argc, char *argv[]) {
-
+	pid_t pid=fork();
+	if (pid == 0) { /* Child Process*/
+		static char *argv[]={"stress-ng", "--matrix", "0", "-t", "30s"};
+		/*execv("stress-ng", argv);*/
+		execv("/usr/bin/stress-ng",argv);
+		printf("Should not get here!\n");
+		exit(127); /* only if execv fails */
+	} else { /* Parent Process */
+		/* PID=TID when there is only one thread for a process */
+		printf("Waiting!\n");
+		waitpid(pid,0,0); /* wait for child to exit */
+	}
+	return EXIT_SUCCESS;
 }
