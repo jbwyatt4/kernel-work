c07d8d4e49cd6486919c0b322b1f5d60740780e9
John B. Wyatt IV <jbwyatt4@gmail.com>
2021-08-12 01:35:44 -0700

    core_timer: mostly implement event pairing

--- core_timer/analyze_trace.py
+++ core_timer/analyze_trace.py
@@ -5,8 +5,30 @@ import bt2
 import sys
 from pprint import pprint
 
+class EventRecord:
+	def __init__(self, event_type, cpu_core, time_stamp):
+		self.event_type = event_type
+		self.cpu_core = cpu_core
+		self.time_stamp = time_stamp
+
 class ParseTrace:
 	CHECKED_EVENTS = ['irq:', 'sched:']
+	cpu_pairs = { # hardcoded, make dynamic later
+		0: 1,
+		1: 0,
+		2: 3,
+		3: 2
+	}
+	events_to_match = { # hardcoded, make dynamic later
+		0: [],
+		1: [],
+		2: [],
+		3: []
+	}
+	found_pairs = { # hardcoded, make dynamic later
+		2: [],
+		4: []
+	}
 
 	def process_main_state(cls):
 		pass
@@ -19,6 +41,23 @@ class ParseTrace:
 
 	def is_sswitch_state(self):
 		return self.state["CURRENT_STATE"] == self.state["STATES"]["SSWITCH_STATE"]
+
+	# get the other cpu id of the pair
+	def get_cpu_pair(self, event, cpu_id):
+		return self.cpu_pairs(event[cpu_id])
+
+	# check if a pair event already exists
+	# if so, remove the paired events and store in a matched value
+	# if not, add the event to a list to compare future events against
+	def check_cpu_pair(self, msg, cpu_id):
+		event = msg.event
+		pair_id = self.get_cpu_pair(event, event["cpu_id"])
+		e = EventRecord(event.name, event["cpu_id"], msg.default_clock_snapshot.ns_from_origin)
+		if 0 < self.events_to_match[pair_id].length:
+			matched = self.events_to_match[pair_id].pop(0)
+			#found_pairs
+		else:
+			self.events_to_match[pair_id].append(e)
 
 	def __init__(self):
 		state = {
@@ -57,21 +96,26 @@ for msg in msg_it:
 
 	# Check if substring s is in event.name
 	# we are looking for 'sched:...', etc
-	if not any(s in event.name for s in ParseTrace.CHECKED_EVENTS):
+	if not any(s in event.name for s in p.CHECKED_EVENTS):
 		unrelated_event_count = unrelated_event_count + 1
 		m = 'Error: Unallowed Event: {}'
 		print(m.format(event.name))
 		exit(1)
 
-	if ParseTrace.CHECKED_EVENTS[0] in event.name:
+	if p.CHECKED_EVENTS[0] in event.name:
 		irq_count = irq_count + 1
 
-	if ParseTrace.CHECKED_EVENTS[1] in event.name:
+	if p.CHECKED_EVENTS[1] in event.name:
 		sched_count = sched_count + 1
 
 	if "sched:sched_switch" == event.name:
 		m = 'CPU of sched_switch: {}'
 		print(m.format(event.packet.context_field["cpu_id"]))
+		print(m.format(event["cpu_id"]))
+		print(event.payload_field.keys)
+		t = msg.default_clock_snapshot.ns_from_origin
+		p.check_cpu_pair(msg, event["cpu_id"])
+
 
 m = """
 Unrelated Event Count: {}
